7a3da618b571fa793362ae74b4b901ece45bd626
Kenneth Lewis <kennethlewis@Kenneths-MacBook-Pro.local>
2017-09-09 11:07:36 -0500

    added functions to dom.js

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,8 +1,42 @@
-console.log("in xhr.js");
-
 var CatStore = ((oldCatStore) => {
 
+	oldCatStore.combineArrays = (cats, owners) => {
+		//combine arrays
+		cats.forEach((cat) => {
+			owners.forEach((owner) => {
+				if(cat.ownerId === owner.id) {
+					cat.owner = owner.name;
+				}
+			});
+		});
+		CatStore.domString(someArray);
+	};
 
+	oldCatStore.domString = (inputArray) => {
+		//write some strings
+		let catz = '';
+        for(let i=0; i<inputArray.length; i++){
+            let newCat = "";
+            newCat+=`<div class="col-xs-3">`
+            newCat+=`<div class="catBox col-xs-12 text-center noPad"  id="catBox-${i}">`;
+            newCat+=`<div class="catName">${inputArray[i].name}</div>`;
+            newCat+=`<img class="catImage" src="${inputArray[i].imageUrl}">`;
+            if(inputArray[i].owner !== "None"){
+                newCat+= `<button class="btn btn-default owner">${inputArray[i].owner}</button>`
+            } else {
+                newCat+= `<button class="btn btn-danger owner" id="adopt-${i}">Adopt Me!</button>`
+            }
+            newCat+= `<div class="catSkill text-center">${inputArray[i].specialSkill}</div>`
+            newCat+= `</div>`
+            newCat+= `</div>`
+            catz += newCat;
+        }
+		CatStore.writeTDom(catz);
+	};
+	oldCatStore.writeTDom = (strang) => {
+		let fancyPetStore = document.getElementById("fancyPetStore");
+		fanceyPetStore.innerHTML = strang;
+	};
 
 	return oldCatStore;
 })(CatStore || {});
